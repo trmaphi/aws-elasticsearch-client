85bd312e0ed65224865b9294dc8ad44b716127c9
trmaphi <trmaphi@gmail.com>
2019-10-19 23:38:55 +0700

    Fix typings

--- AWSConnection.d.ts
+++ AWSConnection.d.ts
@@ -1,5 +1,6 @@
 import * as http from 'http';
 import AWS, { Credentials, AWSError, HttpRequest } from 'aws-sdk';
+import { Connection } from '@elastic/elasticsearch';
 
 interface RequestOptions extends http.ClientRequestArgs {
     asStream?: boolean;
@@ -7,7 +8,7 @@ interface RequestOptions extends http.ClientRequestArgs {
     querystring?: string;
 }
 
-export class AWSConnection {
+export class AWSConnection extends Connection {
     request(params: RequestOptions, callback: (err: Error | null, response: http.IncomingMessage | null) => void): http.ClientRequest;
     _createRequest(params: any): HttpRequest
     _getAWSCredentials(): Promise<Credentials | AWSError>
--- index.d.ts
+++ index.d.ts
@@ -1,8 +1,10 @@
 import { Client, ClientOptions } from '@elastic/elasticsearch';
 import { AWSConnection } from './AWSConnection';
 
-declare module '@trmaphi/aws-elasticsearch-service-client' {
-    export class AWSElasticSearchClient extends Client {
-        constructor(opts: ClientOptions)
-    }
+declare class AWSElasticSearchClient extends Client {
+    constructor(opts: ClientOptions)
+}
+
+export {
+    AWSElasticSearchClient,
 }
